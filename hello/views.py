42a406feff4d23d1052c7057beef2d034cda7fb2
永拓郎 <nagatakurou@eitakurou-no-Mac-mini.local>
2021-05-07 10:00:40 +0900

    Demo4

--- hello/views.py
+++ hello/views.py
@@ -9,9 +9,9 @@ import os
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-     return HttpResponse('ようこそPythonへ!')
+    # return HttpResponse('ようこそPythonへ!')
 
-    #return render(request, "index.html")
+    return render(request, "index.html")
 
     #r = requests.get('http://httpbin.org/status/418')
     #print(r.text)
